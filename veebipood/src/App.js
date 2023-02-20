f9814ba980d273d20190d7b049dc62cb6bae5c52
mihkelvaher <mihkel.vaher@industry62.com>
2023-02-20 12:12:23 +0200

    Uus Reacti projekt + navigeerimine

---
+++ veebipood/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+
+// tag --> <div>   </div>   komplektide jaoks
+// <img /> self-closing    tema omadused lähevadki tema sisse
+// button ---> on algus ja lõpp, tema sisse kirjutatakse midagi
+
+function App() {
+  return (
+    <div className="App">
+      <Link to="/avaleht">
+        <img className="pilt" src="https://nobecars.com/wp-content/uploads/2022/01/Untitled-2-5-1024x473.png" alt="" />
+      </Link>
+
+      <Link to="/ostukorv">
+        <button className="nupp">Nupp</button>
+      </Link>
+
+      <Link to="/lisa-toode">
+        <button className="nupp">Toodet lisama</button>
+      </Link>
+
+      <Routes>
+        <Route path="avaleht" element={<div>123</div>} />
+        <Route path="ostukorv" element={<div>O</div>} />
+        <Route path="lisa-toode" element={<div>LT</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ veebipood/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+// Navigeerimiseks:
+// 1. react-router-dom    --->  laeb internetist alla võimekuse navigeerimsieks (paneb koodi node_modules kausta)
+// 2. index.js failis tuleb BrowserRouter kasutusele võtta
+// 3. App.js failis teeme URL ja sisu seosed
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
